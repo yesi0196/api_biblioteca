674143afcac796cb08e8d4a2a6e170ee4cd49824
Cami <Cami@gmail.com>
2023-08-12 18:51:27 -0300

    configuracion de api

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 
 const express = require('express');
+
+const { auth } = require("express-oauth2-jwt-bearer");
+
+// Importamos el Middleware Error Handler
+const errorHandler = require('./middleware/errorHandler.js');
+
+// Configuracion Middleware con el Servidor de Autorización
+const autenticacion = auth({
+    audience: "http://localhost:3000/api/productos",
+    issuerBaseURL: "https://dev-utn-frc-iaew.auth0.com/",
+    tokenSigningAlg: "RS256",
+});
+
 const app = express();
 app.use(express.json());
 
 // Importamos el Router de Libros
 const librosRouter = require('./routes/libros');
 
-// Importamos el Middleware Error Handler
-const errorHandler = require('./middleware/errorHandler.js');
-
-app.use('/libros', librosRouter);
+//Configuramos el middleware de autenticacion
+app.use('/libros', autenticacion, librosRouter);
 app.use(errorHandler);
 app.listen(3000, () => {
     console.log('Servidor iniciado en el puerto 3000');
